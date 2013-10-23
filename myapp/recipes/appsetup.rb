e03b80195689ad61038906b002b916c8a46ce82d
Oliver Mack <mail@oliver-mack.com>
2013-10-23 22:48:15 +0200

    Some additions

--- myapp/recipes/appsetup.rb
+++ myapp/recipes/appsetup.rb
@@ -16,7 +16,7 @@ node[:deploy].each do |app_name, deploy|
       :user =>     (deploy[:database][:username] rescue nil),
       :password => (deploy[:database][:password] rescue nil),
       :db =>       (deploy[:database][:database] rescue nil),
-      :table =>    (node[:phpapp][:dbtable] rescue nil)
+      :table =>    (node[:myapp][:dbtable] rescue nil)
     )
 
    only_if do
